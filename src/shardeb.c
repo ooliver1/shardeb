debedcec2aebe5663dfd4e487e77044239ab1cd7
ooliver1 <oliverwilkes2006@icloud.com>
2022-06-17 19:28:02 +0100

    feat: large commit as i am tired of this

--- src/shardeb.c
+++ src/shardeb.c
@@ -3,29 +3,23 @@
 #include <ngx_http.h>
 
 typedef struct {
-    ngx_uint_t value;
+    ngx_int_t shards;
+    ngx_int_t clusters;
+    ngx_str_t query_param;
 } shardeb_conf_t;
 
-static ngx_int_t shardeb_filter(ngx_http_request_t *r);
 static void *shardeb_create_loc_conf(ngx_conf_t *cf);
 static char *shardeb_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
-static char *shards(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
-static char *clusters(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static char *shardeb(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
 static ngx_command_t shardeb_commands[] = {
-    {ngx_string("shards"),
-     NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF |
-         NGX_CONF_TAKE2,
-     shards, NGX_HTTP_LOC_CONF_OFFSET, 0, NULL},
-    {ngx_string("clusters"),
-     NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF |
-         NGX_CONF_TAKE2,
-     clusters, NGX_HTTP_LOC_CONF_OFFSET, 0, NULL},
-};
+    {ngx_string("shardeb"), NGX_HTTP_LOC_CONF | NGX_CONF_TAKE3, shardeb,
+     NGX_HTTP_LOC_CONF_OFFSET, 0, NULL},
+    ngx_null_command};
 
 static ngx_http_module_t shardeb_ctx = {
-    NULL, /* preconfiguration */
-    NULL, /* postconfiguration */
+    NULL,         /* preconfiguration */
+    shardeb_init, /* postconfiguration */
 
     NULL, /* create main configuration */
     NULL, /* init main configuration */
@@ -37,57 +31,21 @@ static ngx_http_module_t shardeb_ctx = {
     shardeb_merge_loc_conf   /* merge location configuration */
 };
 
-ngx_module_t shardeb_module = {NGX_MODULE_V1,
-                               &shardeb_ctx,     /* module context */
-                               shardeb_commands, /* module directives */
-                               NGX_HTTP_MODULE,  /* module type */
-                               NULL,             /* init master */
-                               NULL,             /* init module */
-                               NULL,             /* init process */
-                               NULL,             /* init thread */
-                               NULL,             /* exit thread */
-                               NULL,             /* exit process */
-                               NULL,             /* exit master */
-                               NGX_MODULE_V1_PADDING};
+ngx_module_t shardeb_module = {
+    NGX_MODULE_V1,
+    &shardeb_ctx,     /* module context */
+    shardeb_commands, /* module directives */
+    NGX_HTTP_MODULE,  /* module type */
+    NULL,             /* init master */
+    NULL,             /* init module */
+    NULL,             /* init process */
+    NULL,             /* init thread */
+    NULL,             /* exit thread */
+    NULL,             /* exit process */
+    NULL,             /* exit master */
+    NGX_MODULE_V1_PADDING};
 
-/* header filter handler */
-
-// static ngx_int_t shardeb_filter(ngx_http_request_t *r)
-// {
-//     ngx_table_elt_t *h;
-//     shardeb_conf_t *slcf;
-
-//     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-//                    "http set_header handler");
-
-//     slcf = ngx_http_get_module_loc_conf(r, shardeb_module);
-
-//     /*
-//      * if no header is defined for the location,
-//      * proceed to the next header filter in chain
-//      */
-
-//     if (slcf->value == NULL) {
-//         return ngx_http_next_header_filter(r);
-//     }
-
-//     /* add header to output */
-
-//     h = ngx_list_push(&r->headers_out.headers);
-//     if (h == NULL) {
-//         return NGX_ERROR;
-//     }
-
-//     h->hash = 1;
-//     h->value = slcf->value;
-
-//     /* proceed to the next handler in chain */
-
-//     return ngx_http_next_header_filter(r);
-// }
-
-static void *shardeb_create_loc_conf(ngx_conf_t *cf)
-{
+static void *shardeb_create_loc_conf(ngx_conf_t *cf) {
     shardeb_conf_t *conf;
 
     conf = ngx_pcalloc(cf->pool, sizeof(shardeb_conf_t));
@@ -98,48 +56,69 @@ static void *shardeb_create_loc_conf(ngx_conf_t *cf)
     return conf;
 }
 
-static char *shardeb_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
-{
+static char *shardeb_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child) {
     shardeb_conf_t *prev = parent;
     shardeb_conf_t *conf = child;
 
-    if (conf->value == NULL) {
-        conf->value = prev->value;
+    if (conf->query_param.data == NULL) {
+        conf->shards = prev->shards;
+        conf->clusters = prev->clusters;
+        conf->query_param = prev->query_param;
     }
 
     return NGX_CONF_OK;
 }
 
-static char *shards(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
-{
-    shardeb_conf_t *slcf = conf;
+static ngx_int_t shardeb_handler(ngx_http_request_t *r) {
+    shardeb_conf_t *cf = ngx_http_get_module_loc_conf(r, shardeb_module);
+    ngx_str_t *name = &cf->query_param;
+    ngx_int_t *guild = ngx_http_get_variable_index(cf, name);
 
-    ngx_uint_t *value;
+    int shard = (*guild << 22) % cf->shards;
+    int cluster = (shard) / (cf->shards / cf->clusters);
 
-    if (slcf->value) {
+    ngx_http_add_variable(cf, "guild", NGX_HTTP_VAR_CHANGEABLE);
+
+    return NGX_OK;
+}
+
+static char *shardeb(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
+    shardeb_conf_t *config = conf;
+
+    ngx_uint_t *values;
+
+    ngx_int_t *check;
+    ngx_int_t *clusters;
+    ngx_int_t *shards;
+    ngx_str_t *value;
+    ngx_conf_post_t *post;
+
+    check = (ngx_int_t *)(config + cmd->offset);
+
+    if (*check != NGX_CONF_UNSET) {
         return "is duplicate";
     }
 
-    value = cf->args->elts;
+    values = cf->args->elts;
+    *clusters = ngx_atoi(value[2].data, value[2].len);
+    *shards = ngx_atoi(value[2].data, value[2].len);
 
-    slcf->value = value[1];
+    if (*clusters == NGX_ERROR || *shards == NGX_ERROR) {
+        return "invalid number";
+    }
+
+    config->clusters = clusters;
+    config->shards = shards;
+    config->query_param = value[3];
 
     return NGX_CONF_OK;
 }
 
-static char *clusters(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
-{
-    shardeb_conf_t *slcf = conf;
+static char *shardeb_init(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
+    ngx_http_core_loc_conf_t *clcf;
 
-    ngx_uint_t *value;
-
-    if (slcf->value) {
-        return "is duplicate";
-    }
-
-    value = cf->args->elts;
-
-    slcf->value = value[1];
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+    clcf->handler = shardeb_handler;
 
     return NGX_CONF_OK;
 }
